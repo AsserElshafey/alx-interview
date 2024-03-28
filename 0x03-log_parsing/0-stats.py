d5d6db1cbd9d54bc54eb754e9e57b3027a54cad0
Asser Elshafey <elshafeyasser@yahoo.com>
2024-03-28 21:20:32 +0200

    HOT FIX

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,16 +5,13 @@ import sys
 import signal
 
 
-# Define the regular expression pattern for the log line
 log_line_pattern = re.compile(
     r'(\d+\.\d+\.\d+\.\d+) - \[(.*?)\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)'
 )
 
-# Initialize a dictionary to count status codes
 status_code_counts = {str(code): 0 for code in [
     200, 301, 400, 401, 403, 404, 405, 500]}
 
-# Initialize the total file size
 total_file_size = 0
 
 
@@ -32,10 +29,8 @@ def signal_handler(sig, frame):
     sys.exit(0)
 
 
-# Register the signal handler for SIGINT
 signal.signal(signal.SIGINT, signal_handler)
 
-# Process the input line by line
 try:
     for line_number, line in enumerate(sys.stdin, start=1):
         match = log_line_pattern.match(line)
@@ -45,11 +40,9 @@ try:
             if status_code in status_code_counts:
                 status_code_counts[status_code] += 1
 
-        # Print metrics after every 10 lines or on keyboard interruption
         if line_number % 10 == 0:
             print_metrics()
 
 except KeyboardInterrupt:
-    # Handle any other keyboard interruption
     print_metrics()
     sys.exit(0)
