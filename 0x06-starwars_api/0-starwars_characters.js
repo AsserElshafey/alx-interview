4826b45391bc74c0ff774924b31d554add7d566a
Asser Elshafey <elshafeyasser@yahoo.com>
2024-04-18 17:34:09 +0200

    Hot Fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
-
-function getMovieCharacters(movieId) {
-  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-  request(url, (error, response, body) => {
-    if (error) {
-      console.error("Error retrieving movie data:", error);
-      return;
-    }
-
-    if (response.statusCode === 200) {
-      const data = JSON.parse(body);
-      const characters = data.characters;
-
-      characters.forEach(characterUrl => {
-        request(characterUrl, (characterError, characterResponse, characterBody) => {
-          if (characterError) {
-            console.error("Error retrieving character data:", characterError);
-            return;
-          }
-
-          if (characterResponse.statusCode === 200) {
-            const characterData = JSON.parse(characterBody);
-            console.log(characterData.name);
-          } else {
-            console.error(`Error getting character: ${characterUrl}`, characterResponse.statusCode);
-          }
-        });
-      });
-    } else {
-      console.error(`Error retrieving movie data: ${response.statusCode}`);
-    }
-  });
-}
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
-  const movieId = parseInt(process.argv[2]);
-  getMovieCharacters(movieId);
-} else {
-  console.error("Please provide a movie ID as an argument.");
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
 }
